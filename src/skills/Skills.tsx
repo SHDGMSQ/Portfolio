9432b78433783c6caf08ef2b5417d9eaec9f6e1a
SHDGMSQ <sh.dg.msq@mail.ru>
2022-03-22 18:33:33 +0300

    new commit without working

--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -10,9 +10,9 @@ export const Skills = () => {
             <div className={`${styleContainer.container} ${style.skillsContainer}`}>
                 <h2 className={style.title}>Skills</h2>
                 <div className={style.skills}>
-                    <Skill title={'JS'} description={'wekjfnwekjfnwruighrognwoegfnweogjnwogjjernognerognregoerngerijgnerogjnerog'}/>
-                    <Skill title={'HTML'} description={'ghrognwoegfnweognerognregoerngerijgnerogjnerog'}/>
-                    <Skill title={'React'} description={'nerognregonerogjnerog'}/>
+                    <Skill title={'JS'} description='Hi, it is my JS skill1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111'/>
+                    <Skill title={'HTML'} description='Hi, it is my HTML skill22222222222222222222222222222222222222222222'/>
+                    <Skill title={'React'} description='Hi, it is my React skill3333333333333333333333333333333333333333'/>
                 </div>
             </div>
         </div>
