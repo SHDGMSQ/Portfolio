611a7a4e8f41ef2a68c27d245d3f963ab0a80a53
SHDGMSQ <sh.dg.msq@mail.ru>
2022-07-20 18:04:57 +0300

    new commit v3.0

--- src/App.tsx
+++ src/App.tsx
@@ -57,21 +57,6 @@ export const App = () => {
                         <Route path={PATH.CONTACTS} element={<Contacts/>}/>
                         <Route path={PATH.PAGE_NOT_FOUND} element={<Error404/>}/>
                     </Routes>
-
-                    <div className='footer-swiper-pagination'>
-                    <div className='swiper-pagination'>
-                        <span className='swiper-pagination-bullet' style={{opacity: '1', transform: 'translate3d(0px, 0px, 0px)'}}>
-                    <div className='parallax-wrap' style={{transform: 'translate(0px, 0px)'}}>
-                        <div className='parallax-element' style={{transform: 'translate(0px, 0px)'}}>
-                            <svg className='fp-arc-loader' viewBox='0 0 20 20'>
-                                <circle className='path' cx='10' cy={10} r={5.5} fill='none' transform='rotate(-90 10 10)' strokeOpacity='1' strokeWidth='2px'>''</circle>
-                                <circle className='solid-fill' cx='10' cy={10} r={3}>''</circle>
-                            </svg>
-                        </div>
-                    </div>
-                </span>
-                    </div>
-                    </div>
                 </animated.div>
             )}
         </div>
--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -9,9 +9,9 @@ export const Main = () => {
             <div className={style.mainBlock}>
                 <div className={stylesContainer.container}>
                     <div className={style.text}>
-                        <span>Hi There</span>
+                        <span style={{color: 'white'}}>Hi There</span>
                         <h1>I am Dmitry Shavlukevich</h1>
-                        <p>Frontend Developer</p>
+                        <p style={{color: 'white'}}>Frontend Developer</p>
                     </div>
                     <div className={style.photo}></div>
                 </div>
