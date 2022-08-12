21c042c13916ec741b5ee5adc0dc91931f827e7d
SHDGMSQ <sh.dg.msq@mail.ru>
2022-08-12 20:53:04 +0300

    commit with project block_v1.2

--- src/contacts/Contacts.tsx
+++ src/contacts/Contacts.tsx
@@ -4,21 +4,20 @@ import styleContainer from '../common/styles/Container.module.css';
 import {Title} from '../common/components/Title';
 
 
-
 export const Contacts = () => {
     return (
         <>
-            <div className={style.contactsBlock}>
-                <div className={`${styleContainer.container} ${style.contactsContainer}`}>
-                    <Title title={'Contacts'}/>
-                    <form className={style.formContainer}>
-                        <input className={style.input}/>
-                        <input className={style.input}/>
-                        <textarea className={style.input}/>
-                    </form>
-                    <button className={style.button}>Send</button>
+                <div className={style.contactsBlock}>
+                    <div className={`${styleContainer.container} ${style.contactsContainer}`}>
+                        <Title title={'Contacts'}/>
+                        <form className={style.formContainer}>
+                            <input className={style.input}/>
+                            <input className={style.input}/>
+                            <textarea className={style.input}/>
+                        </form>
+                        <button className={style.button}>Send</button>
+                    </div>
                 </div>
-            </div>
         </>
     );
 };
