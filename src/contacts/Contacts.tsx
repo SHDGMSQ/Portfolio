452e65811c7f0c1aa75650f53a46588b713c5dc0
SHDGMSQ <sh.dg.msq@mail.ru>
2022-08-13 19:54:08 +0300

    commit with contacts block_v1.3

--- src/contacts/Contacts.tsx
+++ src/contacts/Contacts.tsx
@@ -7,17 +7,36 @@ import {Title} from '../common/components/Title';
 export const Contacts = () => {
     return (
         <>
-                <div className={style.contactsBlock}>
-                    <div className={`${styleContainer.container} ${style.contactsContainer}`}>
-                        <Title title={'Contacts'}/>
-                        <form className={style.formContainer}>
-                            <input className={style.input}/>
-                            <input className={style.input}/>
-                            <textarea className={style.input}/>
-                        </form>
-                        <button className={style.button}>Send</button>
+            <div className={style.contactsBlock}>
+                <div className={`${styleContainer.container} ${style.contactsContainer}`}>
+                    <Title title={'Contacts'}/>
+                    <div className={style.title}>
+                        <h2>Send me a message</h2>
                     </div>
+                    <form className={style.formContainer}>
+                        <div className={style.nameEmail}>
+                            <div className={style.name}>
+                                <span>Name *</span>
+                                <input/>
+                            </div>
+                            <div className={style.email}>
+                                <span>Email *</span>
+                                <input/>
+                            </div>
+                        </div>
+                        <div className={style.subject}>
+                            <span>Subject *</span>
+                            <input/>
+                        </div>
+                        <div className={style.message}>
+                            <span>Message *</span>
+                            <textarea/>
+                        </div>
+                        {/*<div className={style.button}></div>*/}
+                    </form>
+                    {/*<button className={style.button}>Send</button>*/}
                 </div>
+            </div>
         </>
     );
 };
