e58a69dc6745629222ac97aa78c7e3940c2da1fb
SHDGMSQ <sh.dg.msq@mail.ru>
2022-08-14 18:10:45 +0300

    commit with contacts block_v1.5

--- src/contacts/Contacts.tsx
+++ src/contacts/Contacts.tsx
@@ -6,6 +6,9 @@ import {TbBrandTelegram} from 'react-icons/tb';
 import {FaInstagram} from 'react-icons/fa';
 import {IoLogoLinkedin} from 'react-icons/io';
 import {FaSkype} from 'react-icons/fa';
+import {TiLocationOutline} from 'react-icons/ti';
+import {FiPhoneCall} from 'react-icons/fi';
+import {GoMail} from 'react-icons/go';
 import {IconContext} from 'react-icons';
 
 export const Contacts = () => {
@@ -16,6 +19,11 @@ export const Contacts = () => {
             <div className={style.contactsBlock}>
                 <div className={`${styleContainer.container} ${style.contactsContainer}`}>
                     <Title title={'Contacts'}/>
+                    <div className={style.adressContainer}>
+                        <ContactHeader logo={<TiLocationOutline/>} title={'Address'} description={'Belarus, Minsk'}/>
+                        <ContactHeader logo={<FiPhoneCall/>} title={'Phone'} description={'+375 (29) 582-91-59'}/>
+                        <ContactHeader logo={<GoMail/>} title={'Email'} description={'sh.dg.msq@mail.ru'}/>
+                    </div>
                     <div className={style.title}>
                         <h2>Let's Connect</h2>
                     </div>
@@ -55,9 +63,7 @@ export const Contacts = () => {
                             <span>Message *</span>
                             <textarea/>
                         </div>
-                        {/*<div className={style.button}></div>*/}
                     </form>
-                    {/*<button className={style.button}>Send</button>*/}
                 </div>
             </div>
         </>
@@ -78,8 +84,26 @@ const ContactLabel: React.FC<ContactLabelPropsType> = (
         </IconContext.Provider>
     </>;
 };
+
+const ContactHeader: React.FC<ContactHeaderPropsType> = ({logo, title, description}) => {
+    return <>
+        <IconContext.Provider value={{color: '#f26c4f', size: '3rem'}}>
+        <div className={style.inner}>
+            <div className={style.innerLogo}>{logo}</div>
+            <div className={style.innerTitle}>{title}</div>
+            <div className={style.innerDescription}>{description}</div>
+        </div>
+        </IconContext.Provider>
+    </>
+}
+
 //types
 type ContactLabelPropsType = {
     title: string
     logoComponent: ReactElement
+}
+type ContactHeaderPropsType = {
+    logo: ReactElement
+    title: string
+    description: string
 }
