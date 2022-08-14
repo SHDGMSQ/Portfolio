6b0422e1c6c1d6894cc261a19802816cb98bcc34
SHDGMSQ <sh.dg.msq@mail.ru>
2022-08-14 14:47:38 +0300

    commit with contacts block_v1.4

--- src/contacts/Contacts.tsx
+++ src/contacts/Contacts.tsx
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, {ReactElement} from 'react';
 import style from './Contacts.module.css';
 import styleContainer from '../common/styles/Container.module.css';
 import {Title} from '../common/components/Title';
-
+import {TbBrandTelegram} from 'react-icons/tb';
+import {FaInstagram} from 'react-icons/fa';
+import {IoLogoLinkedin} from 'react-icons/io';
+import {FaSkype} from 'react-icons/fa';
+import {IconContext} from 'react-icons';
 
 export const Contacts = () => {
+
+
     return (
         <>
             <div className={style.contactsBlock}>
                 <div className={`${styleContainer.container} ${style.contactsContainer}`}>
                     <Title title={'Contacts'}/>
+                    <div className={style.title}>
+                        <h2>Let's Connect</h2>
+                    </div>
+                    <div className={style.contactIcons}>
+                        <ContactLabel
+                            title="Telegram" logoComponent={<TbBrandTelegram/>}
+                        />
+                        <ContactLabel
+                            title="Instagram" logoComponent={<FaInstagram/>}
+                        />
+                        <ContactLabel
+                            title="LinkedIn" logoComponent={<IoLogoLinkedin/>}
+                        />
+                        <ContactLabel
+                            title="Skype" logoComponent={<FaSkype/>}
+                        />
+                    </div>
                     <div className={style.title}>
                         <h2>Send me a message</h2>
                     </div>
@@ -40,3 +63,23 @@ export const Contacts = () => {
         </>
     );
 };
+
+const ContactLabel: React.FC<ContactLabelPropsType> = (
+    {title, logoComponent}
+) => {
+    return <>
+        <IconContext.Provider value={{color: 'white', size: '1.5rem'}}>
+            <div className={style.contactIcon}>
+                <div className={style.label}>
+                    {logoComponent}
+                </div>
+                <span className={style.text}>{title}</span>
+            </div>
+        </IconContext.Provider>
+    </>;
+};
+//types
+type ContactLabelPropsType = {
+    title: string
+    logoComponent: ReactElement
+}
