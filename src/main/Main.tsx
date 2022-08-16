c648d08828a49cb7994ae1b700c38a63c938aef8
SHDGMSQ <sh.dg.msq@mail.ru>
2022-08-16 21:42:19 +0300

    commit version 1.2

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, {ReactElement} from 'react';
 import style from './Main.module.css';
 import stylesContainer from './../common/styles/Container.module.css';
+import {IconContext} from 'react-icons';
+import {TbBrandTelegram} from 'react-icons/tb';
+import {FaInstagram, FaSkype} from 'react-icons/fa';
+import {IoLogoLinkedin} from 'react-icons/io';
 
 
 export const Main = () => {
@@ -8,13 +12,46 @@ export const Main = () => {
         <>
             <div className={style.mainBlock}>
                 <div className={stylesContainer.container}>
-
+                    <div className={style.mainContainer}>
+                        <div className={style.contacts}>
+                            <MainContactLabel
+                                logoComponent={<TbBrandTelegram/>}
+                            />
+                            <MainContactLabel
+                                logoComponent={<FaInstagram/>}
+                            />
+                            <MainContactLabel
+                                logoComponent={<IoLogoLinkedin/>}
+                            />
+                            <MainContactLabel
+                                logoComponent={<FaSkype/>}
+                            />
+                        </div>
                         <div className={style.text}>
-                            <h1>I am Dmitry Shavlukevich</h1>
-                            <p className={style.description}>Frontend Developer</p>
+                            <h2>I am Dmitry <span>Shavlukevich</span></h2>
+                            <h2 className={style.description}>Frontend Developer</h2>
                         </div>
                     </div>
                 </div>
+            </div>
         </>
     );
 };
+
+const MainContactLabel: React.FC<MainContactLabelPropsType> = (
+    {logoComponent}
+) => {
+    return <>
+        <IconContext.Provider value={{size: '1.2rem'}}>
+            <div className={style.contactIcon}>
+                <div className={style.label}>
+                    {logoComponent}
+                </div>
+            </div>
+        </IconContext.Provider>
+    </>;
+};
+
+type MainContactLabelPropsType = {
+    logoComponent: ReactElement
+}
