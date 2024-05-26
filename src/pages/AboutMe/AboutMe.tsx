8a2ff3e9f75a11be14ff29827a398dafc6b816ee
SHDGMSQ <sh.dg.msq@mail.ru>
2024-05-27 00:37:48 +0300

    animate description about

--- src/pages/AboutMe/AboutMe.tsx
+++ src/pages/AboutMe/AboutMe.tsx
@@ -33,6 +33,11 @@ export const AboutMe = () => {
     {id: 6, src: "/Portfolio/assets/photos/6.jpg", pt: "100%"},
   ];
 
+  const text = "I am a positive and result oriented front-end developer with experience building landing pages and SPA, using React (JS, TS), Redux, HTML and CSS (SCSS). I speak English at an Intermediate level. In my free time I improve my skills and English. I would like to find a project work/full-time job in a creative company with up-to-date task and collaborative team!"
+
+  const textArr = text.split(" ")
+
+
   return (
     <>
       <div className={style.aboutMe} id="aboutMe">
@@ -42,13 +47,12 @@ export const AboutMe = () => {
             <div className={style.photosContainer}>
               {transformColumns(photos).map((col, index) => (
                 <div className={style.column} key={index}>
-                  {col.map((photo, i) => {
+                  {col.map((photo) => {
                     return (
                       <div className={style.photo} key={photo.id} style={{paddingTop: photo.pt}}>
                         <img src={photo.src}/>
                       </div>
                     );
-
                   })}
                 </div>
               ))}
@@ -56,10 +60,18 @@ export const AboutMe = () => {
             <div className={style.innerDescription}>
               <div className={style.title}><h4>Hi There!</h4></div>
               <div className={style.description}>
-                I am a positive and result oriented front-end developer with experience
-                building landing pages and SPA, using React (JS, TS), Redux, HTML and CSS (SCSS). I speak English at an
-                Intermediate level. In my free time I improve my skills and English. I would like to find a project
-                work/full-time job in a creative company with up-to-date task and collaborative team!
+                {textArr.map((symbol, index) => (
+                  <React.Fragment key={index}>
+                    <span style={{
+                      animationDuration: "0.8s",
+                      animationDelay: `${0.01 * index}s`,
+                      animationTimingFunction: "cubic-bezier(0.11, 0, 0.5, 0)",
+                      animationFillMode: "forwards"
+                    }}>{symbol}{' '}
+                  </span>
+                    {index < textArr.length - 1 && <span key={`space-${index}`}> </span>}
+                  </React.Fragment>
+                ))}
               </div>
               <div className={style.contactsContainer}>
                 <div className={style.contacts}>
